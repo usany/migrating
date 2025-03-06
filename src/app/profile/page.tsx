c4ccfe95da4d8b793119bd5e67e76b5e430a4d0b
usany <ckd_qja@naver.com>
2025-03-06 17:33:28 +0900

    cern

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -23,7 +22,7 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
-        <Link href='/profile'>profile</Link>
+
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
