1eb444de06d2b9a1c2fee0f38fbfb2ec731ac346
usany <ckd_qja@naver.com>
2025-03-06 17:58:57 +0900

    cenr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,6 +23,7 @@ export default function Home() {
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
+
         <Link href='/profile'>profile</Link>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
